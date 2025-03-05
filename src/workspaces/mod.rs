a4eb2919d69218005cb5b470b4c85acaac7e1cc5
Honza Rychly <honza@axiomdatascience.com>
2025-03-05 10:32:47 -0900

    Rename the `init` module

--- src/workspaces/mod.rs
+++ src/workspaces/mod.rs
@@ -1,13 +1,13 @@
 
-mod workspace_id;
-mod init;
-mod switch;
-mod print;
+mod initialize;
 mod move_container;
+mod print;
 mod swap_groups;
+mod switch;
 mod utils;
+mod workspace_id;
 
-pub use init::init_workspaces;
+pub use initialize::init_workspaces;
 pub use switch::switch_workspace_groups;
 pub use print::{subscribe_and_print, print_state_text, print_waybar_module};
 pub use move_container::move_container_to_workspace_group;
